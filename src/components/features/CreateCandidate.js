239d945e815c04d5fadf6601799f74741700eca0
Prashant <prashantyadav50@yahoo.co.in>
2020-07-29 13:18:12 +0530

    Validation Updates

--- src/components/features/CreateCandidate.js
+++ src/components/features/CreateCandidate.js
@@ -25,52 +25,79 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const validationSchema = Yup.object({
-  candidateName: Yup.string().required("Candidate Name is a required field"),
-  onboardingStatus: Yup.string().required(
-    "Onboarding Status is a required field"
-  ),
-  candidateLTIId: Yup.string().required("Candidate LTI Id is a required field"),
-  clientSelectionDate: Yup.date().required(
-    "Candidate Selection date is a required field"
-  ),
-  grade: Yup.string().required("Grade is a required field"),
-  skills: Yup.string().required("Skills is a required field"),
-  totalExp: Yup.number().required(
+  	candidateName: Yup.string()
+      .matches(/^[a-zA-Z '-]+$/, 'Enter valid Candidate Name')
+      .required("Candidate Name is a required field"),
+	  onboardingStatus: Yup.string().required(
+		  "Onboarding Status is a required field"
+	  ),
+    candidateLTIId: Yup.string()
+      	.matches(/^[0-9]+$/, 'Enter valid Candidate LTI Id')
+      	.required("Candidate LTI Id is a required field"),
+  	clientSelectionDate: Yup.date().required(
+    	"Candidate Selection date is a required field"
+  	),
+  grade: Yup.string()
+  	.matches(/^[a-zA-Z0-9]+$/, 'Enter valid Grade')
+  	.required("Grade is a required field"),
+  skills: Yup.string()
+  	.matches(/[a-zA-Z0-9,;.]/g, 'Please remove invalid special characters')
+  	.required("Skills is a required field"),
+  totalExp: Yup.number()
+  .required(
     "Total experience is a required field and should be in months"
   ),
   baseBU: Yup.string().required("Base BU is a required field"),
   clientBU: Yup.string().required("Client BU is a required field"),
   salesPOC: Yup.string().required("Sales POC"),
-  deliveryManager: Yup.string().required(
+  deliveryManager: Yup.string()
+  .matches(/^[a-zA-Z]+$/, 'Please enter valid Delivery manager name')
+  .required(
     "Delivery manager is a required field"
   ),
-  clientHiringManager: Yup.string().required(
+  clientHiringManager: Yup.string()
+  .matches(/^[a-zA-Z]+$/, 'Please enter valid Client hiring manager name')
+  .required(
     "Client hiring manager is a required field"
   ),
-  clientHead: Yup.string().required("Client head is a requied field"),
-  billRate: Yup.string().required("Bill rate is a required field"),
+  clientHead: Yup.string()
+  .matches(/^[a-zA-Z]+$/, 'Please enter valid Client head name')
+  .required("Client head is a requied field"),
+  bgvDate: Yup.date().required(),
+  billRate: Yup.string()
+  .matches(/[a-zA-Z0-9$]/g, 'Please enter valid Bill Rate')
+  .required("Bill rate is a required field"),
   bgvStatus: Yup.string().required(),
   locationStatus: Yup.string().required(),
-  offerReleaseDate: Yup.string().required(),
+  //offerReleaseDate: Yup.string().required(),
   ltiDOJ: Yup.date().required(),
-  clientCToolID: Yup.string().required(),
-  jobCategory: Yup.string().required(),
+  clientCToolID: Yup.string()
+  .matches(/^[a-zA-Z0-9]+$/, 'Please enter valid Client CTool ID'),
+  jobCategory: Yup.string(),
   dcInitiationDate: Yup.date().required(),
   dcClearedDate: Yup.date().required(),
   dcstatus: Yup.string().required(),
   pevStatus: Yup.string().required(),
   techSelectStatus: Yup.string().required(),
   techSelectionDate: Yup.date().required(),
-  ltiWorkCountryName: Yup.string().required(
-    "Base location country is required"
-  ),
-  ltiWorkCityName: Yup.string().required("Base location country is required"),
-  clientWorkCountryName: Yup.string().required(
-    "Client location country is required"
-  ),
-  clientWorkCityName: Yup.string().required(
-    "Client location country is required"
-  ),
+  ltiWorkCountryName: Yup.string()
+  	.matches(/^[a-zA-Z]+$/, 'Please enter valid country name')
+	.required(
+		"Base location country is required"
+	),
+  ltiWorkCityName: Yup.string()
+  	.matches(/^[a-zA-Z]+$/, 'Please enter valid city name')
+  	.required("Base location City is required"),
+  clientWorkCountryName: Yup.string()
+  	.matches(/^[a-zA-Z]+$/, 'Please enter valid client country name')	
+	.required(
+		"Client location country is required"
+	),
+  clientWorkCityName: Yup.string()
+  	.matches(/^[a-zA-Z]+$/, 'Please enter valid client city name')
+	.required(
+		"Client location country is required"
+	),
 });
 
 const formatDate = (date) => {
@@ -346,8 +373,7 @@ function CreateCandidate() {
                       name="offerReleaseDate"
                       label="Offer Release Date"
                       variant="dialog"
-                      format="dd/MM/yyyy"
-                      required
+                      format="dd/MM/yyyy"                      
                     />
                   </Grid>
                   <Grid item lg={5} md={10} sm={10} xs={10}>
@@ -372,15 +398,13 @@ function CreateCandidate() {
                   <Grid item lg={5} md={10} sm={10} xs={10}>
                     <FormikField
                       name="clientCToolID"
-                      label="Client CTool ID"
-                      required
+                      label="Client CTool ID"                      
                     />
                   </Grid>
                   <Grid item lg={5} md={10} sm={10} xs={10}>
                     <FormikField
                       name="positionID"
-                      label="Position ID"
-                      required
+                      label="Position ID"                      
                     />
                   </Grid>
                   <Grid item lg={5} md={10} sm={10} xs={10}>
